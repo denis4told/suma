0d2bd31f5d5e0077608f35cc2fbee2ca9d9c08e1
Denis Rodriguez <denis@joses-mac-mini.local>
2018-03-14 10:03:17 -0400

    Initial Commit

---
+++ Suma/Suma.h
@@ -0,0 +1,19 @@
+//
+//  Suma.h
+//  Suma
+//
+//  Created by Denis Rodriguez on 3/14/18.
+//  Copyright © 2018 Denis Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Suma.
+FOUNDATION_EXPORT double SumaVersionNumber;
+
+//! Project version string for Suma.
+FOUNDATION_EXPORT const unsigned char SumaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Suma/PublicHeader.h>
+
+
